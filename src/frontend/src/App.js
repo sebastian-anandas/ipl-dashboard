03b744422196c23102edb48ad3c3f6559c5db1d6
sebastian-anandas <sebastian.anandas@gmail.com>
2024-04-14 04:21:56 +0530

    Minor bug fix in the routing

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import TeamPage from "./pages/TeamPage";
 import MatchPage from "./pages/MatchPage";
 import HomePage from "./pages/HomePage";
@@ -7,6 +7,9 @@ import HomePage from "./pages/HomePage";
 function App() {
     return (<div className="App">
         <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/teams" />} />
+            </Routes>
             <Routes>
                 <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>}/>
             </Routes>
